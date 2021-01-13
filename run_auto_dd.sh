b0374b418422bf88d503b5115ab39afda68c034e
Ignacio Nacho Fernandez <ignacio.fernandez@king.com>
2021-01-13 15:58:30 +0100

    Trying to improve the UX when running the batch and sh

--- run_auto_dd.sh
+++ run_auto_dd.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# TODO if .env exists then activate, otherwise warn
+if [ -d .env ]; then
+    source ./env/bin/activate
+else
+    echo "WARN: You do not have a virtual environment. Have you ran install_requirements.sh?"
+    echo "Things might explode in a spectacular fashion"
+fi
 
 python -u main.py
+
+if [ -d .env ]; then
+    deactivate
+fi
